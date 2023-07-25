a5f436dec307bca3894cc5cf3ea17624033e1f83
Adam Ginzberg <adam@baserun.ai>
2023-07-25 16:19:59 -0400

    Update cli to leverage pytest, unittests, cli, call baserun.log explicitly

--- baserun/baserun.py
+++ baserun/baserun.py
@@ -1,102 +1,113 @@
-import logging
 import uuid
+import os
 import requests
 import threading
-import contextlib
 import warnings
 import time
 
 _thread_local = threading.local()
 
 
-class BaserunHandler(logging.Handler):
-    def __init__(self, api_url: str, api_key: str) -> None:
-        super().__init__()
-        self.api_url = api_url
-        self.api_key = api_key
-        self.buffer = []
-
-    def flush(self, metadata=None):
-        if not self.buffer:
-            return
-        headers = {"Authorization": f"Bearer {self.api_key}"}
-        requests.post(self.api_url, json={"tests": [{**metadata, "steps": self.buffer}]}, headers=headers)
-        self.buffer = []
-
-    def emit(self, record: logging.LogRecord) -> None:
-        baserun_id = getattr(_thread_local, "baserun_id", None)
-
-        if not baserun_id:
-            raise ValueError("Logs meant for Baserun must be inside a Baserun context manager.")
-
-        baserun_payload = getattr(record, 'baserun_payload', {})
-
-        log_entry = {
-            "message": record.msg,
-            "payload": baserun_payload
-        }
-
-        self.buffer.append(log_entry)
-
-
-class BaserunFilter(logging.Filter):
-    def filter(self, record: logging.LogRecord) -> bool:
-        return hasattr(record, 'baserun_payload')
-
-
-class BaserunTest(contextlib.ContextDecorator):
-    def __init__(self, metadata=None):
-        self.metadata = dict(metadata or {})
-
-    def __call__(self, func):
-        def wrapper(*args, **kwargs):
-            if 'name' not in self.metadata:
-                self.metadata['name'] = func.__name__
-            with self:
-                result = func(*args, **kwargs)
-                self.metadata['result'] = str(result)
-                return result
-
-        return wrapper
-
-    def __enter__(self):
-        if not Baserun._initialized:
-            raise ValueError("Baserun has not been initialized. Please call baserun.init() first.")
-
-        self.metadata['id'] = str(uuid.uuid4())
-        _thread_local.baserun_id = self.metadata['id']
-        self.metadata['startTimestamp'] = time.time()
-
-        return self.metadata['id']
-
-    def __exit__(self, *exc):
-        self.metadata['completionTimestamp'] = time.time()
-
-        del _thread_local.baserun_id
-        logger = logging.getLogger()
-        for handler in logger.handlers:
-            if isinstance(handler, BaserunHandler):
-                handler.flush(self.metadata)
-
-
 class Baserun:
     _initialized = False
+    _tests = []
+    _api_url = None
+    _api_key = None
 
     @staticmethod
-    def init(api_key: str, api_url: str = "https://baserun.ai/api/runs") -> None:
+    def init(api_url: str = "https://baserun.ai/api/runs") -> None:
+        api_key = os.environ.get('BASERUN_API_KEY')
+        if not api_key:
+            raise ValueError("Baserun API key is missing. Ensure the BASERUN_API_KEY environment variable is set.")
+
         if Baserun._initialized:
             warnings.warn("Baserun has already been initialized. Additional calls to init will be ignored.")
             return
 
-        logger = logging.getLogger()
-        handler = BaserunHandler(api_url, api_key)
-        handler.setLevel(logging.INFO)
-
-        handler.addFilter(BaserunFilter())
-        logger.addHandler(handler)
-
+        Baserun._api_url = api_url
+        Baserun._api_key = api_key
         Baserun._initialized = True
 
     @staticmethod
-    def test(metadata=None) -> BaserunTest:
-        return BaserunTest(metadata)
+    def test(func):
+        def wrapper(*args, **kwargs):
+            if not Baserun._initialized:
+                return func(*args, **kwargs)
+
+            test_name = func.__name__
+            test_id = str(uuid.uuid4())
+            _thread_local.baserun_id = test_id
+            _thread_local.buffer = []
+            start_time = time.time()
+
+            try:
+                result = func(*args, **kwargs)
+            except Exception as e:
+                end_time = time.time()
+                Baserun.store_test({
+                    'name': test_name,
+                    'id': test_id,
+                    'error': str(e),
+                    'startTimestamp': start_time,
+                    'completionTimestamp': end_time,
+                    "steps": _thread_local.buffer
+                })
+                _thread_local.buffer = []
+                del _thread_local.baserun_id
+                raise e
+
+            end_time = time.time()
+            Baserun.store_test({
+                'name': test_name,
+                'id': test_id,
+                'result': str(result),
+                'startTimestamp': start_time,
+                'completionTimestamp': end_time,
+                "steps": _thread_local.buffer
+            })
+            _thread_local.buffer = []
+            del _thread_local.baserun_id
+            return result
+
+        return wrapper
+
+    @staticmethod
+    def log(message, payload=None):
+        if not Baserun._initialized:
+            return
+
+        baserun_id = getattr(_thread_local, "baserun_id", None)
+        if not baserun_id:
+            warnings.warn("baserun.log was called outside of a Baserun decorated test. The log will be ignored.")
+            return
+
+        if not hasattr(_thread_local, 'buffer'):
+            _thread_local.buffer = []
+
+        log_entry = {
+            "message": message,
+            "payload": payload
+        }
+        _thread_local.buffer.append(log_entry)
+
+    @staticmethod
+    def store_test(test_data: dict):
+        Baserun._tests.append(test_data)
+
+    @staticmethod
+    def flush():
+        if not Baserun._initialized:
+            warnings.warn("Baserun has not been initialized. No data will be flushed.")
+            return
+
+        if not Baserun._tests:
+            return
+
+        headers = {"Authorization": f"Bearer {Baserun._api_key}"}
+        try:
+            response = requests.post(Baserun._api_url, json={"tests": Baserun._tests}, headers=headers)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            warnings.warn(f"Failed to upload results to Baserun: {str(e)}")
+
+        Baserun._tests = []
