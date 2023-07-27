5785abf7a18bf407dfeed05a8de0bcf021ef986d
Adam Ginzberg <125328015+adam-baserun@users.noreply.github.com>
2023-07-27 15:32:38 -0700

    Remove the need for a decorator in pytest usage (#20)

--- baserun/pytest_plugin.py
+++ baserun/pytest_plugin.py
@@ -1,9 +1,14 @@
 from baserun import Baserun
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def pytest_addoption(parser):
     parser.addoption("--baserun", action="store_true", help="Enable baserun functionality")
     parser.addoption("--baserun-api-url", default="https://baserun.ai/api/v1/runs", help="Baserun API URL")
+    parser.addoption("--no-flush", action="store_true", default=False,
+                     help="do not flush to baserun, even if --baserun is used")
 
 
 def pytest_sessionstart(session):
@@ -14,4 +19,14 @@ def pytest_sessionstart(session):
 
 def pytest_sessionfinish(session):
     if session.config.getoption("--baserun"):
+        if session.config.getoption("--no-flush"):
+            logger.info("Baserun flush disabled by --no-flush option.")
+            return
+
         Baserun.flush()
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--baserun"):
+        for item in items:
+            item.obj = Baserun.test(item.obj)
