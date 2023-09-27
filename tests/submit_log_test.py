adfd1cc91d1cbd69fc0fbec6ceb636243ae52584
Erik Peterson <e@eriklp.com>
2023-09-26 18:34:11 -0700

    Update submit log test for grpc

--- tests/submit_log_test.py
+++ tests/submit_log_test.py
@@ -1,17 +1,15 @@
+import json
 from unittest.mock import patch
 
 import baserun
-from baserun import baserun as baserun_module
 
 
 def test_log_message():
-    with patch("baserun.Baserun.submission_service.SubmitLog") as mock_submit_span:
+    with patch("baserun.Baserun.submission_service.SubmitLog") as mock_submit_log:
         log_name = "TestEvent"
-        event_payload = {"action": "called_api", "value": 42}
-       
         baserun.log(log_name, "whatever")
-        assert mock_submit_span.call_count == 1
-        args, kwargs = mock_submit_span.call_args_list[0]
+        assert mock_submit_log.call_count == 1
+        args, kwargs = mock_submit_log.call_args_list[0]
 
         submit_log_request = args[0]
         assert submit_log_request.log.name == log_name
@@ -20,11 +18,15 @@ def test_log_message():
 
 
 def test_log_with_payload():
-    event_name = "TestEvent"
+    with patch("baserun.Baserun.submission_service.SubmitLog") as mock_submit_log:
+        log_name = "TestEvent"
+        event_payload = {"action": "called_api", "value": 42}
+        baserun.log(log_name, payload=event_payload)
 
-    baserun.log(event_name, payload=event_payload)
+        assert mock_submit_log.call_count == 1
+        args, kwargs = mock_submit_log.call_args_list[0]
 
-    stored_data = baserun_module._thread_local.buffer
-    assert stored_data[0]["stepType"] == "log"
-    assert stored_data[0]["name"] == event_name
-    assert stored_data[0]["payload"] == event_payload
+        submit_log_request = args[0]
+        assert submit_log_request.log.name == log_name
+        assert submit_log_request.log.payload == json.dumps(event_payload)
+        assert submit_log_request.run.name == "test_log_with_payload"
