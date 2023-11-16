ef8268c947be096a0dbfcd5db863cb5cde394104
Erik Peterson <e@eriklp.com>
2023-11-15 16:00:52 -0800

    Fix init import weirdness

--- baserun/__init__.py
+++ baserun/__init__.py
@@ -2,29 +2,29 @@ import os
 
 from .baserun import Baserun
 from .checks import (
-    check as check_method,
-    check_equals as check_equals_method,
-    check_includes as check_includes_method,
+    check as check,
+    check_equals as check_equals,
+    check_includes as check_includes,
 )
 from .sessions import (
-    start_session as start_session_method,
-    end_session as end_session_method,
-    with_session as with_session_method,
-    astart_session as astart_session_method,
-    aend_session as aend_session_method,
+    start_session as start_session,
+    end_session as end_session,
+    with_session as with_session,
+    astart_session as astart_session,
+    aend_session as aend_session,
 )
 from .templates import (
-    format_prompt as format_prompt_method,
-    register_template as register_template_method,
-    aformat_prompt as aformat_prompt_method,
-    aregister_template as aregister_template_method,
-    get_templates as get_templates_method,
-    get_template as get_template_method,
+    format_prompt as format_prompt,
+    register_template as register_template,
+    aformat_prompt as aformat_prompt,
+    aregister_template as aregister_template,
+    get_templates as get_templates,
+    get_template as get_template,
 )
-from .thread_wrapper import baserun_thread_wrapper
+from .thread_wrapper import baserun_thread_wrapper as thread_wrapper
 from .users import (
-    submit_user as submit_user_method,
-    asubmit_user as asubmit_user_method,
+    submit_user as submit_user,
+    asubmit_user as asubmit_user,
 )
 
 init = Baserun.init
@@ -32,22 +32,29 @@ start_trace = Baserun.start_trace
 trace = Baserun.trace
 log = Baserun.log
 evals = Baserun.evals
-thread_wrapper = baserun_thread_wrapper
-with_session = with_session_method
-start_session = start_session_method
-end_session = end_session_method
-astart_session = astart_session_method
-aend_session = aend_session_method
-format_prompt = format_prompt_method
-register_template = register_template_method
-get_template = get_template_method
-get_templates = get_templates_method
-aformat_prompt = aformat_prompt_method
-aregister_template = aregister_template_method
-submit_user = submit_user_method
-asubmit_user = asubmit_user_method
-check = check_method
-check_equals = check_equals_method
-check_includes = check_includes_method
-
 api_key = os.environ.get("BASERUN_API_KEY")
+
+__all__ = [
+    "api_key",
+    "init",
+    "start_trace",
+    "trace",
+    "log",
+    "evals",
+    "thread_wrapper",
+    "with_session",
+    "end_session",
+    "astart_session",
+    "aend_session",
+    "format_prompt",
+    "register_template",
+    "get_templates",
+    "get_template",
+    "aformat_prompt",
+    "aregister_template",
+    "submit_user",
+    "asubmit_user",
+    "check",
+    "check_equals",
+    "check_includes",
+]
